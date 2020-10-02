fb05659233b5e7ccecc614261dea3900445df5e8
makhlayuk <71181184+makhlayuk@users.noreply.github.com>
2020-10-02 18:53:59 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите шаг x");
+            double step = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите диапозон значений x");
+            double start = Convert.ToDouble (Console.ReadLine());
+            double end = Convert.ToDouble(Console.ReadLine());
+            
+            for (; start<=end ; start+=step)
+            {
+                double y = Math.Pow(start, 2) + 0.5*start +2 ;
+                Console.WriteLine($"|x = {start}\t| y = {y}|");
+                Console.WriteLine("__________________");
+            }
+           
+        }
+    }
+}
